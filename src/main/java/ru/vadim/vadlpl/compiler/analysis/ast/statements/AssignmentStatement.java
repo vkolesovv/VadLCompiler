0a49e1827a6727b0f58e2c080a62d600f7b61b59
vkolesovv <108741450+vkolesovv@users.noreply.github.com>
2023-11-15 22:13:04 +0300

    New statement

---
+++ src/main/java/ru/vadim/vadlpl/compiler/analysis/ast/statements/AssignmentStatement.java
@@ -0,0 +1,26 @@
+package ru.vadim.vadlpl.compiler.analysis.ast.statements;
+
+import ru.vadim.vadlpl.compiler.analysis.ast.expressions.Expression;
+import ru.vadim.vadlpl.compiler.lib.Variables;
+import ru.vadim.vadlpl.compiler.lib.values.Value;
+
+public class AssignmentStatement implements Statement {
+    private final String variable;
+    private final Expression expression;
+
+    public AssignmentStatement(String variable, Expression expression) {
+        this.variable = variable;
+        this.expression = expression;
+    }
+
+    @Override
+    public void execute() {
+        final Value result = expression.eval();
+        Variables.setVariable(variable, result);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s = %s", variable, expression);
+    }
+}
